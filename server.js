5fc1844c3cebf06793afebec95b30fae7234ef54
Manav <s223608412@deakin.edu.au>
2023-08-20 23:48:50 +1000

    adding route and folders

--- server.js
+++ server.js
@@ -1,65 +1,17 @@
-let express = require('express');
+var express = require('express');
 let app = express();
 let port = process.env.port || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@cluster0.xw9ua7k.mongodb.net/?retryWrites=true&w=majority";
-let collection;
+require('./database_connection');
+let router = require("./routers/router");
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use('/api/cat', router);
 
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
 
-async function runDB() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      collection = client.db().collection('Cats');
-      console.log(collection);
-    } catch (ex) {
-        console.error(ex);
-    }
-}
-
-function insertCat(cat,callback) {
-    collection.insertOne(cat,callback);
-}
-
-function getAllCats(callback){
-    collection.find({}).toArray(callback);
-}
-
-app.get('/', function (req,res) {
-    res.render('index.html');
-});
-
-app.post('/api/cat', (req,res)=>{
-    let cat = req.body;
-    console.log(cat);
-    insertCat(cat, (err,result) => {
-        if(!err){
-            res.json({statusCode:201,data:result,message:'success'});
-        }
-    });
-});
-
-app.get('/api/cats', (req,res)=>{
-    getAllCats((err,result)=>{
-        console.log(result);
-        if(!err){
-            res.json({statusCode:200,data:result,message:'success'});
-        }
-    });
-});
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('express server started');
-    runDB();
 });
