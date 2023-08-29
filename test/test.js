fb0fc91b231fd2690578351666809589a1a11e6f
Manav <s223608412@deakin.edu.au>
2023-08-29 12:43:44 +1000

    test

---
+++ test/test.js
@@ -0,0 +1,49 @@
+const {
+    expect
+} = require("chai");
+const request = require("request");
+let cat = {
+    title: "Testing Rocket",
+    subTitle: "testing",
+    describtion: "test post",
+    path: "images/r2.jpeg"
+};
+let url = 'http://localhost:3000/api/cat';
+
+
+describe('GET Request', function() {
+    it('returns status 200 to check if api works', function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+        });
+    });
+
+    it('returns data from DB', function(done){
+        request(url, function(error, response, body) {
+            body = JSON.parse(body);
+            expect(body.data).to.be.a('array');
+            done();
+        });
+    });
+});
+
+describe('Post a cat', function(){
+    it('insert a cat to database', function(done){
+        request.post({url:url, form: cat}, function(error,response,body){
+            //  body = JSON.parse(body);
+            // expect(body.message).to.contain('success');
+            done();
+        });
+    });
+});
+
+describe('Delete a cat', function(){
+    it('Delete a cat from database', function(done){
+        request.delete({url:url, form: cat}, function(error,response,body){
+            // body = JSON.parse(body);
+            // expect(body.message).to.contain('removed');
+            done();
+        });
+    });
+});
